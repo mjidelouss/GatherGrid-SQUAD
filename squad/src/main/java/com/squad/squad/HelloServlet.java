05d359412aeff580922e7cdb247ed46dfdcc2a41
hajjou walkadi <walkadihajjou153@gmail.com>
2023-10-17 16:21:26 +0100

    FrontEnd of Sign in and Sign Up

--- squad/src/main/java/com/squad/squad/HelloServlet.java
+++ squad/src/main/java/com/squad/squad/HelloServlet.java
@@ -1,25 +1,39 @@
 package com.squad.squad;
 
 import java.io.*;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello-servlet", loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
         message = "Hello World!";
     }
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+//DEWD>ATC<RN
+    //delete event with date > mn chihaja and ticket count < mn chihaja retun name.
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        /*response.setContentType("text/html");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        out.println("</body></html>");*/
+        this.getServletContext().getRequestDispatcher("/login.jsp").forward(request,response);
     }
 
     public void destroy() {
