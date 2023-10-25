829cef4e2e165b48520fccdcdf805b38c781c450
mjidelouss <mjid.elouss@gmail.com>
2023-10-25 10:50:12 +0100

    sofksdf

--- squad/src/main/java/com/squad/squad/domain/Ticket.java
+++ squad/src/main/java/com/squad/squad/domain/Ticket.java
@@ -14,16 +14,10 @@ public class Ticket {
 
     private Integer availableQuantity;
 
-<<<<<<< HEAD
     @Enumerated(EnumType.STRING)
     private TicketType ticketType;
 
-    @ManyToOne()
-=======
-    private TicketType ticketType;
-
     @ManyToOne
->>>>>>> main
     private Event event;
 
 
@@ -36,17 +30,10 @@ public class Ticket {
         this.availableQuantity = availableQuantity;
         this.ticketType = ticketType;
         this.event = event;
-<<<<<<< HEAD
     }
 
     public void setPrice(Double price) {
         this.price = price;
-    }
-
-    public void setAvailableQuantity(Integer availableQuantity) {
-        this.availableQuantity = availableQuantity;
-=======
->>>>>>> main
     }
 
     public Long getId() {
@@ -87,8 +74,6 @@ public class Ticket {
 
     public void setEvent(Event event) {
         this.event = event;
-<<<<<<< HEAD
-=======
     }
 
 
@@ -101,7 +86,6 @@ public class Ticket {
                 ", ticketType=" + ticketType +
                 ", event=" + event +
                 '}';
->>>>>>> main
     }
 
     @Override
@@ -111,26 +95,4 @@ public class Ticket {
         Ticket ticket = (Ticket) o;
         return Double.compare(ticket.price, price) == 0 && availableQuantity == ticket.availableQuantity && Objects.equals(id, ticket.id) && ticketType == ticket.ticketType && Objects.equals(event, ticket.event);
     }
-
-
-    @Override
-    public String toString() {
-        return "Ticket{" +
-                "id=" + id +
-                ", price=" + price +
-                ", availableQuantity=" + availableQuantity +
-                ", ticketType=" + ticketType +
-                ", event=" + event.getName() +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Ticket ticket = (Ticket) o;
-        return Double.compare(ticket.price, price) == 0 && availableQuantity == ticket.availableQuantity && Objects.equals(id, ticket.id) && ticketType == ticket.ticketType && Objects.equals(event, ticket.event);
-    }
-
-
 }
